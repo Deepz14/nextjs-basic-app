e0ea6d40dbc5a75b237c289fdfc17d5282c46df0
deepak <deepakchandrasekaran14@gmail.com>
2024-01-24 21:47:52 +0530

    header update

--- app/helpers/fetchData.js
+++ app/helpers/fetchData.js
@@ -3,8 +3,7 @@ export default async function FetchData(URI, options) {
     const reqHeader = options ? options : {
         method: 'GET',
         headers: {
-            'Content-Type': 'application/json',
-            "Authorization": "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"
+            'Content-Type': 'application/json'
         }
     };
     const response = await fetch(URI, reqHeader);
